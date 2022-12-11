d8e4bc85cfdfab752f574c7f9fa69774a0af636b
the-gaurav-paul <gauravme1996@gmail.com>
2022-12-11 19:35:00 +0530

    set up passport library for authentication and post-schema for db

---
+++ config/passport-local-strategy.js
@@ -0,0 +1,64 @@
+ const passport = require('passport');
+
+ const LocalStrategy = require('passport-local').Strategy;
+
+ const User = require('../models/user');
+
+ //authentication using passport
+ passport.use(new LocalStrategy({
+    usernameField: 'email'
+    },
+    function(email,password,done){
+            //find user and establish the identity
+        User.findOne({email:email},function(err,user){
+            if(err){
+                console.log('error while finding user--Passport');
+                return done(err);
+            }
+            if(!user || user.password != password){
+                console.log('Invalid username/password');
+                return done(null, false);
+            }
+            return done(null,user);
+            
+        });
+    }
+ ));
+
+ //serializing the user to decide which key is to be kept in the cookie
+ passport.serializeUser(function(user,done){
+    done(null,user.id);
+ });
+
+ //deserializing the user from the key in the cookie
+ passport.deserializeUser(function(id,done){
+    User.findById(id, function(err,user){
+        if(err){
+            console.log('error while finding the user --> passport');
+            return done(err);
+        }
+        return done(null,user);
+     });
+ });
+
+ //check if the user is authenticated
+ passport.checkAuthentication = function(req,res,next){
+    //if the user is signed in, then pass on the request to the next function(controller's action)
+    if(req.isAuthenticated()){
+        return next();
+    }
+    //if the user is not signed in
+    return res.redirect('/users/sign-in');
+ }
+
+ passport.setAuthenticatedUser = function(req,res,next){
+    if(req.isAuthenticated()){
+        // req.user conatins the current signed in user from the signed in cookie
+        // and we are just sending for locals to views
+        res.locals.user = req.user;
+    }
+    next();
+ }
+  
+
+ module.exports = passport;
--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -8,6 +8,9 @@ module.exports.profile = function (req, res) {
 
 //render user signup page
 module.exports.signUp = function (req, res) {
+  if(req.isAuthenticated()){
+    return res.redirect('/users/profile');
+  }
   return res.render("user_sign_up", {
     title: "Sign Up",
   });
@@ -15,6 +18,9 @@ module.exports.signUp = function (req, res) {
 
 //render user signin page
 module.exports.signIn = function (req, res) {
+  if(req.isAuthenticated()){
+    return res.redirect('/users/profile');
+  }
   return res.render("user_sign_in", {
     title: "Sign In",
   });
@@ -50,5 +56,15 @@ module.exports.create = function (req, res) {
 
 //get the sign in data
 module.exports.createSession = function (req, res) {
-  //todo
+   return res.redirect('/');
 };
+
+
+module.exports.destroySession = function(req,res){
+  req.logout(function(err){
+    if(err){
+      return res.redirect('back');
+    }
+  });
+  return res.redirect('/');
+}
